ce8c936e15fc86e2904c5bd2b3f8aed375ae5c42
Ronny Hendrawan <ronny@dealservices.nl>
2014-09-02 11:39:30 +0200

    adding FreeMarker template engine

--- dakon/src/main/java/me/xvronny/web/dakon/Dakon.java
+++ dakon/src/main/java/me/xvronny/web/dakon/Dakon.java
@@ -1,9 +1,15 @@
 package me.xvronny.web.dakon;
 
 import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import me.xvronny.web.dakon.model.Board;
 import me.xvronny.web.dakon.model.Player;
+import spark.ModelAndView;
 import spark.Session;
+import spark.template.freemarker.FreeMarkerEngine;
 
 /**
  * Dakon is the East Javanese name for the Mancala/Lubang Menggali game. "Lubang Menggali" itself 
@@ -20,12 +26,16 @@ public class Dakon {
    public static void main(String[] args) {
       
       get("/dakon", (request, response) -> {
-    	  Session session = request.session(true);
-    	  if (session.attribute("board") == null) {
-    		  session.attribute("board", new Board(new Player("A"), new Player("B"))) ;
-    	  }
-    	  return "Hello Ho Hi Ho!"+session.attribute("board");
-      });
+    	  /*Session session = request.session(true);
+    	  Board board = session.attribute("board");
+    	  if (board == null) {
+    		  board = new Board(new Player("A"), new Player("B"));
+    		  session.attribute("board", board);
+    	  }*/
+    	  Map<String, Object> attributes = new HashMap<>();
+          attributes.put("message", "Hello World!");
+    	  return new ModelAndView(attributes, "dakon.ftl");
+      }, new FreeMarkerEngine());
       
    }
 
--- dakon/src/main/java/me/xvronny/web/dakon/model/Stone.java
+++ dakon/src/main/java/me/xvronny/web/dakon/model/Stone.java
@@ -2,4 +2,6 @@ package me.xvronny.web.dakon.model;
 
 public class Stone {
 
+	
+	
 }
